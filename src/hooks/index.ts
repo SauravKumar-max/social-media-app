7f761f9f150b91784ebe8428d80e32be4481fb27
Saurav Kumar <maxsaurav237@gmail.com>
2022-01-22 22:43:29 +0530

    final commit

---
+++ src/hooks/index.ts
@@ -0,0 +1,8 @@
+export { usePostLike } from "./postLike";
+export { useFollowing } from "./following";
+export { useComments } from "./comments";
+export { useSignup } from "./signup";
+export { useSearchUser } from "./searchUser";
+export { useBookmark } from "./bookmark";
+export { useEditProfile } from "./editProfile";
+export { useRemovePost } from "./removePost";
