160451d8aeab6d2f31ed24be07a34b5547616a29
Saurav Kumar <maxsaurav237@gmail.com>
2022-01-22 21:15:20 +0530

    final commit

---
+++ src/graphql/index.ts
@@ -0,0 +1,8 @@
+export { LOGIN, SIGNUP } from "./auth";
+export { USER, FOLLOW, UNFOLLOW } from "./user";
+export { PROFILE, EDIT_PROFILE } from "./profile";
+export { ADDLIKE, REMOVELIKE } from "./postLike";
+export { COMMENTS, MUTATECOMMENTS } from "./comment";
+export { ADDPOST, REMOVEPOST } from "./post";
+export { ADD_BOOKMARK, REMOVE_BOOKMARK } from "./bookmark";
+export { SEARCH_USER } from "./searchUser";
